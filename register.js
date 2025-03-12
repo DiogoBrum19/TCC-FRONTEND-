1531957b8f63cdec4528075a8b658371c7bbc31a
DiogoBrum19 <130003743+DiogoBrum19@users.noreply.github.com>
2025-03-12 08:55:37 -0300

    register.js

--- register.js
+++ register.js
@@ -46,17 +46,26 @@ async function register(event) {
         gamingTimePreferences: document.getElementById('gamingTimePreferences').value.split(','),
         favoriteGames: Array.from(document.getElementById('favoriteGames').selectedOptions).map(option => option.value)
     };
-    
-    const response = await fetch(`${API_URL}/cadastrar`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-    });
-    
-    if (response.ok) {
-        alert('Cadastro realizado com sucesso!');
-        window.location.href = 'index.html';
-    } else {
-        alert('Erro ao cadastrar.');
+
+    try {
+        const response = await fetch(`${API_URL}/cadastrar`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+
+        if (response.ok) {
+            alert('Cadastro realizado com sucesso!');
+            window.location.href = 'index.html'; // Redirecionar após o cadastro
+        } else {
+            if (response.status === 409) {
+                alert('Este email já está registrado.');
+            } else {
+                alert('Erro ao cadastrar. Tente novamente.');
+            }
+        }
+    } catch (error) {
+        console.error('Erro ao realizar cadastro:', error);
+        alert('Erro de conexão. Tente novamente.');
     }
 }
